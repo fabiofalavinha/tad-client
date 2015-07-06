4fab3f881e11bbda9a775a5c24d16155df317b91
Fábio Augusto Falavinha <fabio.falavinha@gmail.com>
2015-07-06 00:32:20 -0300

    fixed phone type translator on collaborator's list

--- View/Items/CollaboratorViewItem.cs
+++ View/Items/CollaboratorViewItem.cs
@@ -14,7 +14,7 @@ namespace TadManagementTool.View.Items
         public string Email { get { return Wrapper.Email; } }
         public string StartDate { get { return Wrapper.StartDate.HasValue ? Wrapper.StartDate.Value.ToString("dd/MMM/yy") : "N/A"; } }
         public string GenderType { get { return Wrapper.Gender.TransalateGenderType(); } }
-        public string Telephones { get { return string.Join(", ", Wrapper.Telephones.Select(t => string.Format("{0}: ({1}) {2}", t.PhoneType.ToString(), t.AreaCode, t.Number))); } }
+        public string Telephones { get { return string.Join(", ", Wrapper.Telephones.Select(t => string.Format("{0}: ({1}) {2}", t.PhoneType.Translate(), t.AreaCode, t.Number))); } }
         public string Active { get { return Wrapper.Active ? "Sim" : "Não"; } }
 
         public CollaboratorViewItem(Collaborator collaborator)
