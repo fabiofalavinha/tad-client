5c458ad7ceb7c873244346ad54d176d4b7508e67
fabiofalavinha <fabio.falavinha@gmail.com>
2015-07-08 13:19:15 -0300

    fixed focus to login button

--- LoginForm.Designer.cs
+++ LoginForm.Designer.cs
@@ -127,6 +127,7 @@
             // 
             // LoginForm
             // 
+            this.AcceptButton = this.signInButton;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
