32a8b937df29fe358ff25203bb17c076c4e5ef50
luccawt <lwt.lucca@gmail.cim>
2016-10-21 17:54:11 -0200

    added consecration models

--- Model/Consecration.cs
+++ Model/Consecration.cs
@@ -11,11 +11,13 @@ namespace TadManagementTool.Model
         [JsonProperty("id")]
         public  string Id { get; set; }
 
+        [JsonProperty("event")]
         public Event Event { get; set; }
 
         [JsonProperty("elements")]
         public List<Element> Elements { get; set; }
 
+        [JsonProperty("message")]
         public CommunicationMessage Message { get; set; }
     }
 }
